16701fe7250b4e8c81ea2347aeb9feb882d04a8d
Luishiño <pericena12@gmail.com>
2021-02-02 00:29:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <link rel="icon" type="png" sizes="16x16" href="icon.png">
